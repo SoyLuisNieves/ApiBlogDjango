665541aa3bfcf58762e1f6bd7e9fc8dec84c4a70
Luis Fernando Nieves Nieves <luis.fernan.nieves@gmail.com>
2019-04-15 16:30:01 -0500

    Integrating urls in the app posts

--- posts/views.py
+++ posts/views.py
@@ -3,5 +3,20 @@ from django.shortcuts import render
 
 # Create your views here.
 
-def post_home(request):
+def post_list(request):
     return HttpResponse("Home")
+
+def post_create(request):
+    return HttpResponse("Create")
+
+def post_detail(request):
+    return HttpResponse("Detail")
+
+def post_list(request):
+    return HttpResponse("List")
+
+def post_update(request):
+    return HttpResponse("Update")
+
+def post_delete(request):
+    return HttpResponse("Delete")
