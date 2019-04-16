5e900427d58b81f23da3cddfb5bf49face5b4604
Luis Fernando Nieves Nieves <luis.fernan.nieves@gmail.com>
2019-04-16 09:10:38 -0500

    Validating post_detail vie request with decorator get_object_or_404, dynamic url routing and get absolute url with url links

--- posts/views.py
+++ posts/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from .models import Post
 
@@ -9,8 +9,13 @@ def post_list(request):
 def post_create(request):
     return HttpResponse("Create")
 
-def post_detail(request):
-    return HttpResponse("Detail")
+def post_detail(request, id=None):
+    queryset = get_object_or_404(Post, id=id)
+    context_data = {
+        "queryset": queryset,
+        "title": "Detail post"
+    }
+    return render(request, "post_detail.html", context_data)
 
 def post_list(request):
     queryset = Post.objects.all()
