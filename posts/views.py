bfe7048fbc1c1256620c70c1d454f30f83fa72ff
Luis Fernando Nieves Nieves <luis.fernan.nieves@gmail.com>
2019-04-18 22:34:44 -0500

    Basic users permissions ti create, delete and update post

--- posts/views.py
+++ posts/views.py
@@ -2,13 +2,15 @@ from urllib.parse import quote_plus
 
 from django.contrib import messages
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseRedirect, Http404
 from django.shortcuts import render, get_object_or_404, redirect
 
 from .forms import PostForm
 from .models import Post
 
 def post_create(request):
+    if not request.user.is_staff or not request.user.is_superuser:
+        raise Http404
     form = PostForm(request.POST or None, request.FILES or None)
     if form.is_valid():
         instance = form.save(commit=False)
@@ -53,6 +55,8 @@ def post_list(request):
     return render(request, "post_list.html", context)
 
 def post_update(request, id=None):
+    if not request.user.is_staff or not request.user.is_superuser:
+        raise Http404
     instance = get_object_or_404(Post, id=id)
     form =PostForm(request.POST or None, request.FILES or None, instance=instance)
     if form.is_valid():
@@ -68,6 +72,8 @@ def post_update(request, id=None):
     return render(request, "create_form.html", context)
 
 def post_delete(request, id=None):
+    if not request.user.is_staff or not request.user.is_superuser:
+        raise Http404
     instance = get_object_or_404(Post, id=id)
     instance.delete()
     messages.success(request, "Successfully deleted")
