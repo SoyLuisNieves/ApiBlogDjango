7f6b222144d720ff0cecb71cad65c70a9f87a711
Luis Fernando Nieves Nieves <luis.fernan.nieves@gmail.com>
2019-05-01 15:16:52 -0500

    Implementing django pagedown

--- blogdjango/settings.py
+++ blogdjango/settings.py
@@ -38,6 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # Third
+    'pagedown',
+
+    # Local apps
     'posts',
 ]
 
