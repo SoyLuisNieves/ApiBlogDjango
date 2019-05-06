7cf80fb8fa484393d8ddf59edb5d49f089126e27
Luis Fernando Nieves Nieves <luis.fernan.nieves@gmail.com>
2019-05-06 09:06:00 -0500

    GenericForeignKey and ContentType was added to model Comment

--- comments/models.py
+++ comments/models.py
@@ -1,6 +1,8 @@
 from __future__ import unicode_literals
 
 from django.conf import settings
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
 from django.db import models
 
 from posts.models import Post
@@ -8,6 +10,11 @@ from posts.models import Post
 class Comment(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, default=1)
     post = models.ForeignKey(Post)
+
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.PositiveIntegerField()
+    content_object = GenericForeignKey('content_type', 'object_id')
+    
     content = models.TextField()
     timestamp = models.DateTimeField(auto_now_add=True)
 
