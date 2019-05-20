e47ac83ddb5e789fdcc7c0f975be27d47e7b64ab
Luis Fernando Nieves Nieves <luis.fernan.nieves@gmail.com>
2019-05-20 18:12:55 -0500

    Function register user is Working

--- accounts/views.py
+++ accounts/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth import (
     authenticate,
     get_user_model,
@@ -6,7 +6,7 @@ from django.contrib.auth import (
     logout,
 )
 
-from .forms import UserLoginForm
+from .forms import UserLoginForm, UserRegisterForm
 
 def login_view(request):
     print(request.user.is_authenticated())
@@ -17,8 +17,7 @@ def login_view(request):
         password = form.cleaned_data.get("password")
         user = authenticate(username=username, password=password)
         login(request, user)
-        print(request.user.is_authenticated())
-    
+        return redirect("/")
     context = {
         "form": form,
         "title": title,
@@ -26,7 +25,26 @@ def login_view(request):
     return render(request, "form.html", context)
 
 def register_view(request):
-    return render(request, "form.html", {})
+    print(request.user.is_authenticated())
+    title = "Register"
+    form = UserRegisterForm(request.POST or None)
+    if form.is_valid():
+        user = form.save(commit=False)
+        password = form.cleaned_data.get('password')
+        user.set_password(password)
+        user.save()
+        # First authenticate
+        new_user = authenticate(username=user.username, password=password)
+        # Later login user
+        login(request, new_user)
+        return redirect("/")
+        
+    context = {
+        "form": form,
+        "title": title,
+    }
+    return render(request, "form.html", context)
+    
 def logout_view(request):
     logout(request)
     return render(request, "form.html", {})
