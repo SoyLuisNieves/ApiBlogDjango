c8c129d15783c6f68ee925bd2fadc18ef9984a31
Luis Fernando Nieves Nieves <luis.fernan.nieves@gmail.com>
2019-06-10 10:46:57 -0500

    Retrieve API View- DetailView, Created a new serializer to PostDetail

--- posts/api/urls.py
+++ posts/api/urls.py
@@ -2,6 +2,7 @@ from django.urls import re_path, path
 from django.contrib import admin
 
 from .views import (
+	PostDetailAPIView,
 	PostListAPIView,
 )
 
@@ -9,4 +10,6 @@ app_name = 'posts-api'
 
 urlpatterns = [
 	path('', PostListAPIView.as_view(), name='list'),
+    re_path('(?P<pk>\d+)/$', PostDetailAPIView.as_view(), name='detail'),
+
 ]
--- posts/api/views.py
+++ posts/api/views.py
@@ -1,8 +1,18 @@
-from rest_framework.generics import ListAPIView
+from rest_framework.generics import ListAPIView, RetrieveAPIView
 
 from posts.models import Post
-from posts.api.serializers import PostSerializer
+from posts.api.serializers import PostDetailSerializer, PostListSerializer
 
+
+# Detail Post
+class PostDetailAPIView(RetrieveAPIView):
+	queryset = Post.objects.all()
+	serializer_class = PostDetailSerializer
+	# lookup_field = 'slug'
+	# Slug field as id or pk in url
+	# lookup_url_kwarg = 'abc'
+
+# All post
 class PostListAPIView(ListAPIView):
 	queryset = Post.objects.all()
-	serializer_class = PostSerializer
+	serializer_class = PostListSerializer
