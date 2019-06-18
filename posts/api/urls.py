94016d078f2db3ca8a37a52270eb98c604a1f06e
Luis Fernando Nieves Nieves <luis.fernan.nieves@gmail.com>
2019-06-17 19:18:00 -0500

    The PostUpdateAPIView and PostDeltePIView classes were created within views.py oh the api app, they work correctly when you delete and update a post

--- posts/api/urls.py
+++ posts/api/urls.py
@@ -2,14 +2,17 @@ from django.urls import re_path, path
 from django.contrib import admin
 
 from .views import (
+	PostDeleteAPIView,
 	PostDetailAPIView,
 	PostListAPIView,
+	PostUpdateAPIView,
 )
 
 app_name = 'posts-api'
 
 urlpatterns = [
 	path('', PostListAPIView.as_view(), name='list'),
-    re_path('(?P<pk>\d+)/$', PostDetailAPIView.as_view(), name='detail'),
-
+    re_path(r'^(?P<slug>[-\w]+)/$', PostDetailAPIView.as_view(), name='detail'),
+    re_path(r'^(?P<slug>[-\w]+)/edit/$', PostUpdateAPIView.as_view(), name='update'),
+    re_path(r'^(?P<slug>[-\w]+)/delete/$', PostDeleteAPIView.as_view(), name='delete'),
 ]
--- posts/api/views.py
+++ posts/api/views.py
@@ -1,4 +1,9 @@
-from rest_framework.generics import ListAPIView, RetrieveAPIView
+from rest_framework.generics import (
+	DestroyAPIView,
+	ListAPIView, 
+	RetrieveAPIView,
+	UpdateAPIView,
+)
 
 from posts.models import Post
 from posts.api.serializers import PostDetailSerializer, PostListSerializer
@@ -8,9 +13,20 @@ from posts.api.serializers import PostDetailSerializer, PostListSerializer
 class PostDetailAPIView(RetrieveAPIView):
 	queryset = Post.objects.all()
 	serializer_class = PostDetailSerializer
-	# lookup_field = 'slug'
+	lookup_field = 'slug'
 	# Slug field as id or pk in url
 	# lookup_url_kwarg = 'abc'
+
+class PostUpdateAPIView(UpdateAPIView):
+	queryset = Post.objects.all()
+	serializer_class = PostDetailSerializer
+	lookup_field = 'slug'
+
+
+class PostDeleteAPIView(DestroyAPIView):
+	queryset = Post.objects.all()
+	serializer_class = PostDetailSerializer
+	lookup_field = 'slug'
 
 # All post
 class PostListAPIView(ListAPIView):
